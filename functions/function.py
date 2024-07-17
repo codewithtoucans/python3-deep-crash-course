f271655c34f4408a499a21201349dabb61d1fa86
codegzy-rabbit <2570713180@qq.com>
2024-07-18 01:06:57 +0800

    closures
    closures & decorators intro

--- functions/function.py
+++ functions/function.py
@@ -47,7 +47,7 @@ print(a, b, c, d)
 
 # keyword arguments
 # function parameters after *, must be keyword arguments
-def hello(*, d):
+def hellol(*, d):
     pass
 
 
@@ -80,3 +80,90 @@ add_item(2)
 item2 = add_item(3)
 print(item1)
 print(item2)
+
+
+# Docstrings
+# stored in the function's __doc__ property
+# use help() function
+def hello(a, b, /):
+    """say hello"""
+    print("hello")
+
+
+print(hello.__doc__)
+
+
+# function annotations
+# stored in the __annotations__ property of the function
+# type hinting
+def world(a: int) -> None:
+    """world function"""
+    print(a)
+
+
+print(world.__annotations__)
+
+# Lambda Expressions
+# lambda expressions are anonymous functions
+# lambda expressions are used to create short, one-line functions
+# lambda [parameters] : expression, the expression return a function object
+
+# the function body of lambda is limited to a single expression
+# no assignments
+# no annotations or type hinting
+# single logical line of code
+square = lambda x: x**2
+print(type(square))
+print(square(5))
+
+
+# functions introspection
+# functions are first-class objects, they have attributes, we can attach our own attributes
+# functions can be assigned to variables
+# functions can be passed as arguments
+# functions can be returned from other functions
+# functions can be stored in data structures
+hello.category = "world"
+print(hello.category)
+
+# the dir() function returns a list of the attributes of an object
+# __name__ name of function
+# __defaults__ tuple containing positional parameter defaults
+# __kwdefaults__ dictionary containing keyword parameter defaults
+# __code__ the code object associated with the function, has various properties,
+# like co_varnames, co_argcount(not containing *args and **kwargs), etc.
+print(dir(hello))
+print(hello.__defaults__)
+print(hello.__kwdefaults__)
+print(hello.__code__.co_varnames)
+print(hello.__code__.co_argcount)
+
+
+# The inspect module
+import inspect
+
+print(inspect.getsource(hello))
+print(inspect.getmodule(hello))
+print(inspect.getcomments(hello))
+print(inspect.signature(hello))
+
+for param in inspect.signature(hello).parameters.values():
+    print(param.name, param.default, param.annotation, param.kind)
+
+
+# callables
+# objects that can be called will return True
+print(callable(print))
+
+# High Order Functions
+# functions that take other functions as arguments
+# functions that return other functions
+# functions that return functions
+# eg: map sorted filter reduce zip
+
+# partial function to reduce the parameters
+
+# built-in reducing functions
+# eg: min max sum any all
+
+# the operator module
--- main.py
+++ main.py
@@ -1,85 +1,153 @@
-# Docstrings
-# stored in the function's __doc__ property
-# use help() function
-def hello(a, b, /):
-    """say hello"""
-    print("hello")
+# Scopes and Namespaces
+
+# Global scopes are nested inside the built-in scope
+
+# The global keyword
 
 
-print(hello.__doc__)
+a = 0
 
 
-# function annotations
-# stored in the __annotations__ property of the function
-# type hinting
-def world(a: int) -> None:
-    """world function"""
+def my_func():
+    # global a
+    a = 100
     print(a)
 
 
-print(world.__annotations__)
-
-# Lambda Expressions
-# lambda expressions are anonymous functions
-# lambda expressions are used to create short, one-line functions
-# lambda [parameters] : expression, the expression return a function object
-
-# the function body of lambda is limited to a single expression
-# no assignments
-# no annotations or type hinting
-# single logical line of code
-square = lambda x: x**2
-print(type(square))
-print(square(5))
+my_func()
+print(a)
 
 
-# functions introspection
-# functions are first-class objects, they have attributes, we can attach our own attributes
-# functions can be assigned to variables
-# functions can be passed as arguments
-# functions can be returned from other functions
-# functions can be stored in data structures
-hello.category = "world"
-print(hello.category)
+# The nonloacl keyword
+def outer_func():
+    x = "hello"
 
-# the dir() function returns a list of the attributes of an object
-# __name__ name of function
-# __defaults__ tuple containing positional parameter defaults
-# __kwdefaults__ dictionary containing keyword parameter defaults
-# __code__ the code object associated with the function, has various properties,
-# like co_varnames, co_argcount(not containing *args and **kwargs), etc.
-print(dir(hello))
-print(hello.__defaults__)
-print(hello.__kwdefaults__)
-print(hello.__code__.co_varnames)
-print(hello.__code__.co_argcount)
+    def inner_fun():
+        # nonlocal x
+        x = "world"
+        y = 100
+        print(y)
+
+    inner_fun()
+    print(x)
 
 
-# The inspect module
-import inspect
-
-print(inspect.getsource(hello))
-print(inspect.getmodule(hello))
-print(inspect.getcomments(hello))
-print(inspect.signature(hello))
-
-for param in inspect.signature(hello).parameters.values():
-    print(param.name, param.default, param.annotation, param.kind)
+outer_func()
 
 
-# callables
-# objects that can be called will return True
-print(callable(print))
+# Closures
+# You can think of the closure as a function plus an extended scope that contains the free variables
 
-# High Order Functions
-# functions that take other functions as arguments
-# functions that return other functions
-# functions that return functions
-# eg: map sorted filter reduce zip
 
-# partial function to reduce the parameters
+# The label x is in two different scopes but always reference the same 'value'
+# python does this by creating a cell as an intermediary object
+# in effect, both variables x (in outer and inner), point to the same cell
+# when requesting the value of the variable, python will 'double-hop' to get to the final value
+def outer():
+    x = "python"
 
-# built-in reducing functions
-# eg: min max sum any all
+    def inner():
+        print(x)
 
-# the operator module
+    return inner
+
+
+fn = outer()
+# introspection
+print(fn.__code__.co_freevars)
+print(fn.__closure__)
+
+
+def adder(n):
+    def inner(x):
+        return x + n
+
+    return inner
+
+
+a = adder(10)
+b = adder(20)
+
+print(a(5))
+print(b(5))
+print(a.__closure__)
+print(b.__closure__)
+
+# this is a error example
+adders = []
+for n in range(1, 4):
+    adders.append(lambda x: x + n)
+
+print(adders[0](10))
+print(adders[1](10))
+print(adders[2](10))
+print(adders[0].__closure__)
+print(adders[1].__closure__)
+print(adders[2].__closure__)
+print(adders[0].__code__.co_freevars)
+print(adders[1].__code__.co_freevars)
+print(adders[2].__code__.co_freevars)
+
+
+# closure applications
+# simplify a class with a method that needs to access the class instance
+class Averager:
+    def __init__(self) -> None:
+        self.numbers = []
+
+    def add(self, number):
+        self.numbers.append(number)
+        return sum(self.numbers) / len(self.numbers)
+
+
+a = Averager()
+print(a.add(10))
+print(a.add(20))
+print(a.add(30))
+
+
+def average() -> callable:
+    numbers = []
+
+    def inner(number: int) -> float:
+        numbers.append(number)
+        return sum(numbers) / len(numbers)
+
+    return inner
+
+
+ave = average()
+print(ave(10))
+print(ave(20))
+print(ave(30))
+
+
+# closures and decorators
+def add(a, b):
+    return a + b
+
+
+def mult(a, b):
+    return a * b
+
+
+def counter(fn, c):
+    count = 0
+
+    def inner(*args, **kwargs):
+        nonlocal count
+        count += 1
+        c[fn.__name__] = count
+        return fn(*args, **kwargs)
+
+    return inner
+
+
+c = {}
+add = counter(add, c)
+mult = counter(mult, c)
+print(add(10, 20))
+print(mult(10, 20))
+print(add(10, 20))
+print(mult(10, 20))
+print(c)
